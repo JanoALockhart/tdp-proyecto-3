0200372a28356d128d75426d559a4659a562eeb3
Jano Axel Lockhart <janolockhart@gmail.com>
2021-10-30 20:24:06 -0300

    Creadas clases visitors

---
+++ src/logica/entidades/visitadores/VisitadorPerseguidor.java
@@ -0,0 +1,34 @@
+package logica.entidades.visitadores;
+
+import logica.entidades.Collectibles.Collectible;
+import logica.entidades.Entornos.Entorno;
+import logica.entidades.Personaje.Jugadores.Jugador;
+import logica.entidades.Personaje.Perseguidores.Perseguidor;
+
+public class VisitadorPerseguidor implements Visitor{
+
+	@Override
+	public void serAfectadoPor(Perseguidor per) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serAfectadoPor(Collectible col) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serAfectadoPor(Jugador player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serAfectadoPor(Entorno ent) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}

aad363158369fc849fec42283df66813931f5f8e
Jano Axel Lockhart <janolockhart@gmail.com>
2021-11-02 18:39:04 -0300

    Modificado constructor de partida para recibir una fabrica

--- src/logica/partida/Partida.java
+++ src/logica/partida/Partida.java
@@ -3,6 +3,9 @@
  */
 package logica.partida;
 
+import logica.builder.*;
+import logica.fabricas.*;
+import logica.mapa.Mapa;
 import vista.UserInterface;
 
 /** 
@@ -10,12 +13,15 @@ import vista.UserInterface;
  */
 public class Partida {
 	private UserInterface pantalla;
+	private BuilderNivel builder;
+	private Mapa miMapa;
 	private int puntaje;
 	
 	
-	public Partida(UserInterface ui) {
+	public Partida(UserInterface ui,FabricaElementos fab) {
 		pantalla = ui;
 		puntaje = 0;	
+		builder = new ArquitectoNivel(fab);
 	}
 	
 	public void siguienteNivel() {

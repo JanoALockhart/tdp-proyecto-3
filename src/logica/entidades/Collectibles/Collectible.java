b3167930ab5ccaabbd958d802bb6a68ca3f4a791
Valentin Dupre <valentindupre79@gmail.com>
2021-11-03 19:50:09 -0300

    Mapa en contrusctores

--- src/logica/entidades/Collectibles/Collectible.java
+++ src/logica/entidades/Collectibles/Collectible.java
@@ -2,11 +2,12 @@ package logica.entidades.Collectibles;
 
 import logica.entidades.*;
 import logica.mapa.Celda;
+import logica.mapa.Mapa;
 
 public abstract class Collectible extends Entidad {
 	public abstract void activarEfecto();
 	
-	public Collectible(String img, int width, int height, Celda c) {
+	public Collectible(String img, int width, int height, Celda c, Mapa m) {
 		super(img, width, height, c);
 	}
 }
--- src/logica/entidades/Collectibles/Puntos/Fruta.java
+++ src/logica/entidades/Collectibles/Puntos/Fruta.java
@@ -5,8 +5,8 @@ import logica.mapa.*;
 
 public class Fruta extends Punto {
 	
-	public Fruta(int val, String img, Celda c) {
-		super(img ,c.getAncho()/3 ,c.getAlto()/3,c);
+	public Fruta(int val, String img, Celda c, Mapa m) {
+		super(img ,c.getAncho()/3 ,c.getAlto()/3,c,m);
 		valor = val;
 	}
 	

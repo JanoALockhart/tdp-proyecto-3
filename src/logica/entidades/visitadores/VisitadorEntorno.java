5a5fd7ea75debe0974fe083ee415488ccecd4ffd
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2021-11-21 19:30:06 -0300

    Van soluciones al timer asustado y bla bla

--- src/logica/entidades/visitadores/VisitadorEntorno.java
+++ src/logica/entidades/visitadores/VisitadorEntorno.java
@@ -4,13 +4,16 @@ import logica.entidades.Collectibles.Collectible;
 import logica.entidades.Entornos.Entorno;
 import logica.entidades.Personaje.Jugadores.Jugador;
 import logica.entidades.Personaje.Perseguidores.Perseguidor;
+import logica.partida.Partida;
+import main.Main;
 
 public class VisitadorEntorno implements Visitor{
 
 	@Override
 	public void serAfectadoPor(Perseguidor per) {
-		// TODO Auto-generated method stub
-		
+		Partida.getInstance().addPuntaje(Integer.parseInt(Main.personajesConfig.getProperty("puntajeAlMorir")));
+		per.morir();
+		//TODO Chequear esto si no es con el state.activarEfecto desde fantasma
 	}
 
 	@Override
